6238da0cae057c889dcb872ec7645010a2a499e1
juliush.dev <juliush.dev@gmail.com>
2025-12-14 08:59:17 +0000

    feat: remove landing shell and default to spa

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,13 +2,5 @@ import { ensureTfLoaded } from './utils/loadTf.js';
 
 await ensureTfLoaded();
 
-const useNewAppFlag = (import.meta.env?.VITE_PLAYGROUND_APP || '').toLowerCase();
-const useNewApp = useNewAppFlag === 'next' || useNewAppFlag === 'new';
-
-if (useNewApp) {
-  const { bootstrapNewApp } = await import('./app/bootstrap.js');
-  bootstrapNewApp();
-} else {
-  await import('./main.js');
-  await import('./landing.js');
-}
+const { bootstrapNewApp } = await import('./app/bootstrap.js');
+bootstrapNewApp();
